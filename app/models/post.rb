746c94412c33e17329d2b2dd092bd3812be7688b
Rebekah Zhou <rebekahzhou@gmail.com>
2022-03-18 10:08:05 -0700

    commit tree 8310ba27c7e1c52c0fb9dcbb901d819f12249fc1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    # validate :clickbaity
+
+    # def clickbaity 
+    #     found = %w(Won't Believe Secret Top [number] Guess).find do |word|
+    #         title.include?(word)
+    #     end
+    #     unless found 
+    #         errors.add(:title, "title isn't sufficiently clickbait-y")
+    #     end
+    # end
 end
